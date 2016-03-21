8d760d4039839d3a589c2ae40246103c8c213a1e
David Arvelo <david@davidarvelo.com>
2016-03-21 00:06:12 -0400

    Reorganize JS file structure

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,2 +1,69 @@
-import EtherWebsite from './root/index';
+import { RootApp, makeOutlet } from 'ether';
+import GettingStartedApp from './apps/getting-started/index';
+import IndexRoute from './routes/index';
+import NavBarRoute from './routes/navbar';
+
+function navbarLinksData() {
+    return [
+        {
+            text: 'Home',
+            address: 'index',
+        },
+        {
+            text: 'Getting Started',
+            address: 'gettingStartedIndex',
+        },
+    ];
+}
+
+class EtherWebsite extends RootApp {
+    expectedOutlets() {
+        return ['main'];
+    }
+    createOutlets(outlets) {
+        outlets.main = makeOutlet({
+            el: outlets.main.get(),
+            classNames: ['main-app'],
+            append: [
+                outlets.navbar = makeOutlet({
+                    tagName: 'nav',
+                    classNames: ['navbar'],
+                    mutable: true,
+                }),
+                outlets.index = makeOutlet({
+                    tagName: 'section',
+                    classNames: ['index'],
+                    mutable: true,
+                }),
+                outlets.gettingStarted = makeOutlet({
+                    tagName: 'section',
+                    classNames: ['getting-started'],
+                    mutable: true,
+                }),
+            ]
+        });
+        return outlets;
+    }
+    mount() {
+        return {
+            '': IndexRoute
+                    .addresses('index')
+                    .outlets('index'),
+            'getting-started':
+                GettingStartedApp
+                    .addresses('gettingStarted')
+                    .outlets('gettingStarted'),
+        };
+    }
+    mountConditionals() {
+        return {
+            '*':
+                NavBarRoute
+                    .addresses('navbar')
+                    .outlets('navbar')
+                    .setup(navbarLinksData),
+        };
+    }
+}
+
 export default EtherWebsite;
