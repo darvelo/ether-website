c194f35bddf4a6bd448ebcf097c4f1ce57f46fe2
David Arvelo <david@davidarvelo.com>
2016-04-02 18:58:03 -0400

    Add Mutable/Outlet docs

--- src/js/main/apps/docs/templates/mutable-outlet.js
+++ src/js/main/apps/docs/templates/mutable-outlet.js
@@ -1,5 +1,20 @@
 export default function() {
     return `
 <h1>MutableOutlet</h1>
+<p>A MutableOutlet's purpose is to wrap a DOM element and proxy access to it and its methods and properties. It also allows direct access to the element itself, and the ability to wrap a different element instead.</p>
+
+<h2>Constructor</h2>
+<p><code>new MutableOutlet(element)</code> &mdash; Takes a DOM element and stores it internally.</p>
+
+
+<h2>Instance Methods</h2>
+<p>All instance methods are the same as in the <code>Outlet</code> class.</p>
+
+
+<h2>Properties</h2>
+<ul>
+    <li><code>el</code> &mdash; Allows getting or setting the wrapped DOM element.</li>
+    <li><code>innerHTML</code> &mdash; Allows getting or setting the element's innerHTML.</li>
+</ul>
     `;
 }
--- src/js/main/apps/docs/templates/outlet.js
+++ src/js/main/apps/docs/templates/outlet.js
@@ -1,5 +1,26 @@
 export default function() {
     return `
 <h1>Outlet</h1>
+<p>An Outlet's purpose is to wrap a DOM element and proxy access to it and to select methods and properties.</p>
+
+<h2>Constructor</h2>
+<p><code>new Outlet(element)</code> &mdash; Takes a DOM element and stores it internally.</p>
+
+
+<h2>Instance Methods</h2>
+<p>These are all proxies for the DOM element's own methods.</p>
+<ul>
+    <li><code>appendChild()</code></li>
+    <li><code>removeChild()</code></li>
+    <li><code>querySelector()</code></li>
+    <li><code>querySelectorAll()</code></li>
+</ul>
+
+
+<h2>Properties</h2>
+<p>These are all proxies for the DOM element's own properties.</p>
+<ul>
+    <li><code>innerHTML</code> &mdash; Only allows getting the element's innerHTML, but not setting it.</li>
+</ul>
     `;
 }
